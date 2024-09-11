f3cd776b39023e0e0157d707b9d30930adb742f0
Frank de Jong <frankdejong@epo.org>
2024-09-11 10:49:58 +0200

    Update GWAudit.lua
    - Added two-phase audit, to prevent players that only just logged in from appearing as if they don't have GreenWall.

--- GWAudit.lua
+++ GWAudit.lua
@@ -1,15 +1,14 @@
 local f=CreateFrame("Frame")
 local c=CreateFrame("Frame")
-local GuildRosterReady
-local gwChannelId,gwChannelCount
+local guildRosterReady
+local gwChannelId
 local outputMode
-local memberList = {}
+local onlineGuildies = {}
 local CTL = _G.ChatThrottleLib
 
 -- Main function called when /gwaudit slash command is given
 
 function GWAudit( slashcmd )
-    local numChannels
 
     -- Handle command line args
     outputMode = nil
@@ -31,9 +30,9 @@ function GWAudit( slashcmd )
     end
 
     -- See if guild roster info has been received, and if so, request GW channel info
-    if GuildRosterReady then
+    if guildRosterReady then
         print("|cFF00FF00GWAudit:|r Auditing GreenWall members.")
-        numChannels=GetNumDisplayChannels()
+        local numChannels = GetNumDisplayChannels()
         local gwChannelName = gw.config.channel.guild.name
         SetSelectedDisplayChannel(4);
         for i=1,numChannels do
@@ -42,11 +41,9 @@ function GWAudit( slashcmd )
                 gwChannelId = i;
                 if GetSelectedDisplayChannel() ~= gwChannelId then
                     c:RegisterEvent("CHANNEL_ROSTER_UPDATE");
-                    SetSelectedDisplayChannel(gwChannelId);
+                    SetSelectedDisplayChannel(gwChannelId);     -- GWAuditProcess() called by callback for CHANNEL_ROSTER_UPDATE
                 else
                     ChannelRoster_Update(gwChannelId);
-                    gwChannelCount = GetNumChannelMembers(gwChannelId);
-                    print(gwChannelCount);
                     GWAuditProcess();
                 end
             end
@@ -62,13 +59,13 @@ end
 
 -- Comms function that outputs the list of guildies without GW to gchat or whispers
 
-local function GWAuditDoComms()
+local function GWAuditDoComms( nonGWMemberList )
 
     -- Send out the message
     if outputMode == "gchat" then
-        if #memberList > 0 then
-            local message = table.remove( memberList )
-            for _, member in ipairs(memberList) do
+        if #nonGWMemberList > 0 then
+            local message = table.remove( nonGWMemberList )
+            for _, member in ipairs(nonGWMemberList) do
                 message = member .. ", " .. message
             end
             print("Guild message to " .. message )
@@ -78,7 +75,7 @@ local function GWAuditDoComms()
                         "GUILD" )
         end
     elseif outputMode == "whisper" then
-        for _, member in ipairs(memberList) do
+        for _, member in ipairs(nonGWMemberList) do
             print("Whisper to " .. member )
             CTL:SendChatMessage("BULK",
                 "GWAUDIT",
@@ -90,48 +87,82 @@ local function GWAuditDoComms()
     end
 end
 
+-- Worker function that double checks if the "non-GW" people on the first pass are still
+-- not on the GW channel
+
+local function GWAuditProcessPhaseTwo()
+
+    -- Add all names in the greenwall channel roster to the table
+    print( "|cFF00FF00GWAudit:|r Retrieving GW channel roster on channel " .. gwChannelId );
+    local gwChannelRoster = {}
+    local gwChannelCount = GetNumChannelMembers( gwChannelId );
+    for i = 1, gwChannelCount do
+        local memberName = C_ChatInfo.GetChannelRosterInfo( gwChannelId, i );
+        gwChannelRoster[ memberName ] = 1
+    end
+
+    -- Prune all the guildies that have logged out in the last 10 seconds
+    local stillOnlineGuildies = {}
+    local guildCount = GetNumGuildMembers()
+    for i = 1, guildCount do
+        local guildieName,_,_,_,_,_,_,_,online = GetGuildRosterInfo(i)
+        local localName, _ = strsplit( "-", guildieName, 2)
+        if online and onlineGuildies[ guildieName ] then
+            table.insert( stillOnlineGuildies, localName )
+        else
+            print("|cFF00FF00GWAudit:|r " .. localName .. " logged out -- skipping")
+        end
+    end
+
+    -- Compare the online guildies from 10 second ago with the current GW channel roster
+    print( "|cFF00FF00GWAudit:|r Performing audit" );
+    local nonGWMemberList = {}
+    for i, v in ipairs( stillOnlineGuildies ) do
+        if gwChannelRoster[ v ] == 1 then
+            table.insert( nonGWMemberList, v )
+        end
+    end
+
+    -- Do comms
+    GWAuditDoComms( nonGWMemberList )
+
+    -- Done
+    print("|cFF00FF00GWAudit:|r Audit complete.")
+
+end
 
 
 -- Worker function that compares people in guild roster with people in global GW channel
 
 local function GWAuditProcess()
 
-    -- Add all names in the greenwall channel roster to the table
-    local gwChannelRoster={}
-    for i=1,gwChannelCount do
-        local memberName = C_ChatInfo.GetChannelRosterInfo(gwChannelId,i);
-        table.insert(gwChannelRoster,memberName);
-    end
-
-    -- Go through all guild members that are online and see if they appear in the table
-    memberList = {}
-    local channelNameStr=table.concat(gwChannelRoster,",")
-    local guildCount=GetNumGuildMembers()
-    for i=1,guildCount do
-        local guildieName,_,_,_,_,_,_,_,online=GetGuildRosterInfo(i)
+    -- Go through all guild members and add the ones that are online to the table;
+    -- we check this against the people that are in the GW channel in 10 seconds.
+    onlineGuildies = {}
+    local guildCount = GetNumGuildMembers()
+    for i = 1, guildCount do
+        local guildieName,_,_,_,_,_,_,_,online = GetGuildRosterInfo(i)
         if online then
-            local localname,localrealm=strsplit("-",guildieName,2)
-            if channelNameStr:find(localname)==nil then
-                print(localname);
-                table.insert( memberList, localname )
-            end
+            local localName, _ = strsplit( "-", guildieName, 2)
+            onlineGuildies[ localName ] = 1
         end
     end
-    GWAuditDoComms()
-    print("|cFF00FF00GWAudit:|r Audit complete.")
+
+    print("|cFF00FF00GWAudit:|r Guild roster retrieval complete, pausing 10 seconds...")
+    C_Timer.After(10.0, GWAuditProcessPhaseTwo )
+
 end
 
 -- Callback for when GuildRoster() request is made in GWAudit()
 f:RegisterEvent("GUILD_ROSTER_UPDATE")
 f:SetScript("OnEvent",function(self,event)
-	GuildRosterReady = true
+	guildRosterReady = true
 	f:UnregisterEvent("GUILD_ROSTER_UPDATE")
 end)
 
 -- Callback for when channel change request is made in GWAudit()
 c:SetScript("OnEvent",function(self,event,id,count)
     if gwChannelId and id == gwChannelId then
-        gwChannelCount=GetNumChannelMembers(id);
         c:UnregisterEvent("CHANNEL_ROSTER_UPDATE");
         GWAuditProcess();
     end
